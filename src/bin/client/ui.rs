5faf266dae7e4265c12bca5f945af7ca23240175
alessandro90 <alessandro.ceccato.3@gmail.com>
2024-06-02 19:56:28 +0200

    Minimal UI implementation

---
+++ src/bin/client/ui.rs
@@ -0,0 +1,56 @@
+use std::net::TcpStream;
+
+use cursive::event::Event;
+use cursive::{
+    event::Key,
+    theme::Theme,
+    view::{Nameable, Resizable, ScrollStrategy, Scrollable},
+    views::{DummyView, EditView, LinearLayout, TextArea, TextView},
+};
+
+const CHAT_VIEW_NAME: &str = "chat_view";
+const INPUT_NAME: &str = "input_view";
+
+pub fn run() {
+    // need Arc<Mutex> here
+    // let stream = TcpStream::connect("127.0.0.1:60000").expect("Unable to connect to server");
+    let mut siv = cursive::default();
+    siv.set_theme(Theme::terminal_default());
+    siv.add_global_callback(Key::Esc, |s| s.quit());
+    // siv.set_global_callback(Event::Refresh, |_| {});
+    siv.set_on_pre_event(Event::CtrlChar('s'), |s| {
+        let text = s.call_on_name(INPUT_NAME, |input: &mut TextArea| {
+            input.get_content().to_owned()
+        });
+        if let Some(text) = text {
+            if !text.is_empty() {
+                s.call_on_name(CHAT_VIEW_NAME, |view: &mut TextView| {
+                    view.append("\n\n");
+                    view.append(text);
+                });
+                s.call_on_name(INPUT_NAME, |input: &mut EditView| input.set_content(""));
+            }
+        }
+        s.call_on_name(INPUT_NAME, |input: &mut TextArea| input.set_content(""));
+    });
+    let screen = LinearLayout::vertical()
+        .child(
+            TextView::new("")
+                .with_name(CHAT_VIEW_NAME)
+                .full_width()
+                .full_height()
+                .scrollable()
+                .scroll_strategy(ScrollStrategy::StickToBottom),
+        )
+        .child(DummyView)
+        .child(
+            TextArea::new()
+                .content("")
+                .with_name(INPUT_NAME)
+                .full_width()
+                .scrollable()
+                .scroll_strategy(ScrollStrategy::StickToBottom),
+        );
+    siv.add_fullscreen_layer(screen);
+    siv.run();
+}
